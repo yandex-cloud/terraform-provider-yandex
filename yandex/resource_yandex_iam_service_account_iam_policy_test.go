3ec518491a9bc48349384dcaca5891686b8b1537
Anatoly Pilikov <zverre@yandex-team.ru>
2021-04-07 07:49:43 +0000

    Merge pull request #301 in CLOUD/terraform-provider-yandex-mirror from feature/fix_full_set4 to master
    Squashed commit of the following:
    
    commit 5d4e85e55c5d83e2bc0e7aa8845da12f5af76f98
    Author: Anatoly Pilikov <zverre@yandex-team.ru>
    Date:   Wed Apr 7 10:36:43 2021 +0300
    
        Fix test

--- yandex/resource_yandex_iam_service_account_iam_policy_test.go
+++ yandex/resource_yandex_iam_service_account_iam_policy_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
-
 	"github.com/yandex-cloud/go-genproto/yandex/cloud/iam/v1"
 )
 
@@ -16,14 +15,14 @@ func TestAccServiceAccountIamPolicy(t *testing.T) {
 	cloudID := getExampleCloudID()
 	serviceAccountName := acctest.RandomWithPrefix("tf-test")
 	userID := getExampleUserID2()
-	role := "resource-manager.clouds.member"
+	role := "editor"
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccServiceAccountIamPolicy_basic(cloudID, serviceAccountName, userID),
+				Config: testAccServiceAccountIamPolicy_basic(cloudID, serviceAccountName, userID, role),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckYandexIAMServiceAccountExistsWithID("yandex_iam_service_account.test_account", &serviceAccount),
 					testAccCheckServiceAccountIam("yandex_iam_service_account.test_account", role, []string{"userAccount:" + userID}),
@@ -42,7 +41,7 @@ func TestAccServiceAccountIamPolicy(t *testing.T) {
 }
 
 //revive:disable:var-naming
-func testAccServiceAccountIamPolicy_basic(cloudID, accountName, userID string) string {
+func testAccServiceAccountIamPolicy_basic(cloudID, accountName, userID, role string) string {
 	prerequisiteMembership, deps := testAccCloudAssignCloudMemberRole(cloudID, userID)
 	return prerequisiteMembership + fmt.Sprintf(`
 resource "yandex_iam_service_account" "test_account" {
@@ -52,7 +51,7 @@ resource "yandex_iam_service_account" "test_account" {
 
 data "yandex_iam_policy" "foo" {
   binding {
-    role    = "resource-manager.clouds.member"
+    role    = "%s"
     members = ["userAccount:%s"]
   }
 }
@@ -63,5 +62,5 @@ resource "yandex_iam_service_account_iam_policy" "foo" {
 
   depends_on = [%s]
 }
-`, accountName, userID, deps)
+`, accountName, role, userID, deps)
 }
