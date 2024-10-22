c507535c646e8d31e5d7ec3c7f62c74392856118
Ivan Romanenko <iva-romanenko@yandex-team.ru>
2024-10-22 13:28:28 +0000

    Pull request #1421: fix docs generation
    Merge in CLOUD/terraform-provider-yandex-mirror from fix-generation to master
    
    Squashed commit of the following:
    
    commit 6c4453e526c8ac1acda1655c2256d7dd34126349
    Author: iva-romanenko <iva-romanenko@yandex-team.ru>
    Date:   Tue Oct 22 14:05:08 2024 +0200
    
        fix docs generation

--- tools/cmd/pkg/categories/categories.go
+++ tools/cmd/pkg/categories/categories.go
@@ -4,11 +4,13 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v3"
 	"os"
+	"sort"
 	"strings"
 )
 
 type CategoryMapping struct {
 	Mapping map[string]string `yaml:"CategoryMapping"`
+	Order   []string
 }
 
 func (c *CategoryMapping) LoadCategoriesMapping(categoriesFile string) error {
@@ -20,12 +22,19 @@ func (c *CategoryMapping) LoadCategoriesMapping(categoriesFile string) error {
 	if err != nil {
 		return fmt.Errorf("failed to unmarshal categories.yaml: %w", err)
 	}
+	ordered := make([]string, 0, len(c.Mapping))
+	for k, _ := range c.Mapping {
+		ordered = append(ordered, k)
+	}
+	sort.Sort(sort.Reverse(sort.StringSlice(ordered)))
+	c.Order = ordered
 	return nil
 
 }
 
 func (c *CategoryMapping) GetSubCategoryByPath(path string) (string, error) {
-	for key, value := range c.Mapping {
+	for _, key := range c.Order {
+		value := c.Mapping[key]
 		if strings.Contains(path, "/"+key+"/") {
 			return value, nil
 		}
@@ -34,7 +43,7 @@ func (c *CategoryMapping) GetSubCategoryByPath(path string) (string, error) {
 }
 
 func (c *CategoryMapping) GetCategoryFromResource(resourceName string) (string, error) {
-	for key, _ := range c.Mapping {
+	for _, key := range c.Order {
 		if strings.HasPrefix(resourceName, strings.ReplaceAll(key, "/", "_")+"_") || key == resourceName {
 			return key, nil
 		}
