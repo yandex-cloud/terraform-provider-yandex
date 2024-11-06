127db7a44147a31f0b530151a9c4645aa6c59b4c
Denis Volkov <denchick@yandex-team.ru>
2024-11-06 15:57:53 +0000

    fix sweeper

--- yandex-framework/test/vpc/security_group/resource_yandex_vpc_security_group_test.go
+++ yandex-framework/test/vpc/security_group/resource_yandex_vpc_security_group_test.go
@@ -46,6 +46,12 @@ func init() {
 	})
 }
 
+// TestMain - add sweepers flag to the go test command
+// important for sweepers run.
+func TestMain(m *testing.M) {
+	resource.TestMain(m)
+}
+
 func testSweepVPCSecurityGroups(_ string) error {
 	conf, err := test.ConfigForSweepers()
 	if err != nil {
