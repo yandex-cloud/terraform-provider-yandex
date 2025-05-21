d29341a7e25b3f8d8ec9902109b5f4fc0286aa13
Sergey Uzhakov <uzhas@ydb.tech>
2025-05-21 19:23:27 +0300

    check connection on server side

--- yandex/resource_yandex_yq_object_storage_connection_test.go
+++ yandex/resource_yandex_yq_object_storage_connection_test.go
@@ -1,14 +1,17 @@
 package yandex
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
+	"github.com/ydb-platform/ydb-go-genproto/draft/protos/Ydb_FederatedQuery"
 )
 
 func TestAccYandexYQObjectStorageConnection_basic(t *testing.T) {
+	connectionName := "my-conn"
 	connectionResourceName := "my-connection"
 	existingConnectionResourceName := fmt.Sprintf("yandex_yq_object_storage_connection.%s", connectionResourceName)
 	resource.Test(t, resource.TestCase{
@@ -17,9 +20,9 @@ func TestAccYandexYQObjectStorageConnection_basic(t *testing.T) {
 		// CheckDestroy: testYandexYDBDatabaseServerlessDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccYQObjectStorageConnectionConfig(connectionResourceName),
+				Config: testAccYQObjectStorageConnectionConfig(connectionName, connectionResourceName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccYQObjectStorageConnectionExists(existingConnectionResourceName),
+					testAccYQObjectStorageConnectionExists(connectionName, existingConnectionResourceName),
 				),
 			},
 			{
@@ -31,17 +34,18 @@ func TestAccYandexYQObjectStorageConnection_basic(t *testing.T) {
 	})
 }
 
-func testAccYQObjectStorageConnectionConfig(connectionResourceName string) string {
+func testAccYQObjectStorageConnectionConfig(connectionName string, connectionResourceName string) string {
 	return fmt.Sprintf(`
 	resource "yandex_yq_object_storage_connection" "%s" {
-        name = "my_cnn_name"
+        name = "%s"
         bucket = "my_bucket"
     }`,
 		connectionResourceName,
+		connectionName,
 	)
 }
 
-func testAccYQObjectStorageConnectionExists(connectionResourceName string) resource.TestCheckFunc {
+func testAccYQObjectStorageConnectionExists(connectionName string, connectionResourceName string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		prs, ok := s.RootModule().Resources[connectionResourceName]
 		if !ok {
@@ -50,6 +54,22 @@ func testAccYQObjectStorageConnectionExists(connectionResourceName string) resou
 		if prs.Primary.ID == "" {
 			return fmt.Errorf("%s", "no ID for connection is set")
 		}
+
+		config := testAccProvider.Meta().(*Config)
+		req := &Ydb_FederatedQuery.DescribeConnectionRequest{
+			ConnectionId: prs.Primary.ID,
+		}
+
+		response, err := config.yqSdk.Client().DescribeConnection(context.Background(), req)
+		if err != nil {
+			return err
+		}
+
+		actualConnectionName := response.Connection.Content.Name
+		if actualConnectionName != connectionName {
+			return fmt.Errorf("invalid connection name %s, expected %s", actualConnectionName, connectionName)
+		}
+
 		return nil
 	}
 }
