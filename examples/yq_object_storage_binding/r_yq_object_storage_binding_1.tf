6ab2369f56356f97be253c92b0f0e38e9f8104f0
Sergey Uzhakov <uzhas@ydb.tech>
2025-05-24 17:58:26 +0300

    add examples for bindings

---
+++ examples/yq_object_storage_binding/r_yq_object_storage_binding_1.tf
@@ -0,0 +1,39 @@
+//
+// Create a new Object Storage binding.
+//
+
+resource "yandex_yq_object_storage_binding" "my_os_binding" {
+    name = "tf-test-os-binding"
+    description = "Binding has been created from Terraform"
+    connection_id = yandex_yq_object_storage_connection.my_os_connection.id
+    format = "csv_with_names"
+    path_pattern = "my_logs/"
+    format_setting = {
+      "file_pattern" = "*.csv"
+    }
+    column {
+      name="year"
+      type="int32"
+      not_null = true
+   }
+    column {
+      name="month"
+      type="int32"
+      not_null = true
+   }
+    column {
+      name="day"
+      type="int32"
+      not_null = true
+   }
+
+   partitioned_by = ["year", "month", "day"]
+   column {
+        name = "ts"
+        type = "Timestamp"
+    }
+    column {
+        name = "message"
+        type = "utf8"
+    }
+}
