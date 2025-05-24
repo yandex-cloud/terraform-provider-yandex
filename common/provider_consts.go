4dd84f6e5c83b95757a285460354eba9a69bd708
Sergey Uzhakov <uzhas@ydb.tech>
2025-05-24 14:58:49 +0300

    fix yq endpoint doc

--- common/provider_consts.go
+++ common/provider_consts.go
@@ -12,7 +12,7 @@ const (
 var Descriptions = map[string]string{
 	"endpoint": "The endpoint for API calls, default value is **" + DefaultEndpoint + "**.\n" +
 		"This can also be defined by environment variable `YC_ENDPOINT`.",
-    "yq_endpoint": "The API endpoint for Yandex.Cloud YQ SDK client, default value is **" + DefaultYQEndpoint + "**.\n" +
+    "yq_endpoint": "The Yandex Query API endpoint, default value is **" + DefaultYQEndpoint + "**.\n" +
         "This can also be defined by environment variable `YQ_ENDPOINT`.",
 	"folder_id": "The ID of the [Folder](https://yandex.cloud/docs/resource-manager/concepts/resources-hierarchy#folder) to operate under, if not specified by a given resource.\n" +
 		"This can also be specified using environment variable `YC_FOLDER_ID`.",
