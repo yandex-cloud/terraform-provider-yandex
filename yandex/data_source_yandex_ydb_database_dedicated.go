8390059a2b5f6158b52f8f4de583a8fe29a3c338
zkpo <zkpo@yandex-team.com>
2025-09-29 20:16:20 +0300

    Fix for `ydb_database_dedicated` data_source error commit_hash:c546eee0312ce04d1c66eebd0735cb3bdb646a9b

--- yandex/data_source_yandex_ydb_database_dedicated.go
+++ yandex/data_source_yandex_ydb_database_dedicated.go
@@ -6,205 +6,32 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/yandex-cloud/go-genproto/yandex/cloud/ydb/v1"
 	"github.com/yandex-cloud/go-sdk/sdkresolvers"
-	"github.com/yandex-cloud/terraform-provider-yandex/common"
 )
 
 func dataSourceYandexYDBDatabaseDedicated() *schema.Resource {
-	return &schema.Resource{
-		Description: "Get information about a Yandex Database (dedicated) cluster. For more information, see [the official documentation](https://yandex.cloud/docs/ydb/concepts/serverless_and_dedicated).\n\n~> If `database_id` is not specified `name` and `folder_id` will be used to designate Yandex Database cluster.\n",
+	dataSource := convertResourceToDataSource(resourceYandexYDBDatabaseDedicated())
 
-		Read: dataSourceYandexYDBDatabaseDedicatedRead,
+	dataSource.Read = dataSourceYandexYDBDatabaseDedicatedRead
 
-		SchemaVersion: 0,
+	dataSource.Description = "Get information about a Yandex Database (dedicated) cluster. For more information, see [the official documentation](https://yandex.cloud/docs/ydb/concepts/serverless_and_dedicated).\n\n~> If `database_id` is not specified `name` and `folder_id` will be used to designate Yandex Database cluster.\n"
 
-		Schema: map[string]*schema.Schema{
-			"database_id": {
-				Type:        schema.TypeString,
-				Description: "ID of the Yandex Database cluster.",
-				Optional:    true,
-			},
+	dataSource.Schema["name"].Optional = true
+	dataSource.Schema["name"].Computed = false
 
-			"name": {
-				Type:        schema.TypeString,
-				Description: common.ResourceDescriptions["name"],
-				Optional:    true,
-			},
+	dataSource.Schema["folder_id"].Optional = true
+	dataSource.Schema["folder_id"].Computed = false
 
-			"network_id": {
-				Type:        schema.TypeString,
-				Description: common.ResourceDescriptions["network_id"],
-				Computed:    true,
-			},
+	dataSource.Schema["deletion_protection"].Optional = true
 
-			"subnet_ids": {
-				Type:        schema.TypeSet,
-				Description: common.ResourceDescriptions["subnet_ids"],
-				Computed:    true,
-				Elem:        &schema.Schema{Type: schema.TypeString},
-				Set:         schema.HashString,
-			},
-
-			"security_group_ids": {
-				Type:        schema.TypeSet,
-				Description: common.ResourceDescriptions["security_group_ids"],
-				Computed:    true,
-				Elem:        &schema.Schema{Type: schema.TypeString},
-				Set:         schema.HashString,
-			},
-
-			"resource_preset_id": {
-				Type:        schema.TypeString,
-				Description: resourceYandexYDBDatabaseDedicated().Schema["resource_preset_id"].Description,
-				Computed:    true,
-			},
-
-			"scale_policy": {
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"fixed_scale": {
-							Type:     schema.TypeList,
-							Computed: true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"size": {
-										Type:     schema.TypeInt,
-										Computed: true,
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-
-			"storage_config": {
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"storage_type_id": {
-							Type:     schema.TypeString,
-							Computed: true,
-						},
-						"group_count": {
-							Type:     schema.TypeInt,
-							Computed: true,
-						},
-					},
-				},
-			},
-
-			"location": {
-				Type:     schema.TypeList,
-				Computed: true,
-				Elem: &schema.Resource{
-					Schema: map[string]*schema.Schema{
-						"region": {
-							Type:     schema.TypeList,
-							Computed: true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"id": {
-										Type:     schema.TypeString,
-										Computed: true,
-									},
-								},
-							},
-						},
-						"zone": {
-							Type:     schema.TypeList,
-							Computed: true,
-							Elem: &schema.Resource{
-								Schema: map[string]*schema.Schema{
-									"id": {
-										Type:     schema.TypeString,
-										Computed: true,
-									},
-								},
-							},
-						},
-					},
-				},
-			},
-
-			"location_id": {
-				Type:        schema.TypeString,
-				Description: resourceYandexYDBDatabaseDedicated().Schema["location_id"].Description,
-				Computed:    true,
-			},
-
-			"assign_public_ips": {
-				Type:        schema.TypeBool,
-				Description: resourceYandexYDBDatabaseDedicated().Schema["assign_public_ips"].Description,
-				Computed:    true,
-			},
-
-			"folder_id": {
-				Type:        schema.TypeString,
-				Description: common.ResourceDescriptions["folder_id"],
-				Optional:    true,
-			},
-
-			"description": {
-				Type:        schema.TypeString,
-				Description: common.ResourceDescriptions["description"],
-				Computed:    true,
-			},
-
-			"labels": {
-				Type:        schema.TypeMap,
-				Description: common.ResourceDescriptions["labels"],
-				Computed:    true,
-				Elem:        &schema.Schema{Type: schema.TypeString},
-				Set:         schema.HashString,
-			},
-
-			"ydb_full_endpoint": {
-				Type:        schema.TypeString,
-				Description: resourceYandexYDBDatabaseDedicated().Schema["ydb_full_endpoint"].Description,
-				Computed:    true,
-			},
-
-			"ydb_api_endpoint": {
-				Type:        schema.TypeString,
-				Description: resourceYandexYDBDatabaseDedicated().Schema["ydb_api_endpoint"].Description,
-				Computed:    true,
-			},
-
-			"database_path": {
-				Type:        schema.TypeString,
-				Description: resourceYandexYDBDatabaseDedicated().Schema["database_path"].Description,
-				Computed:    true,
-			},
-
-			"tls_enabled": {
-				Type:        schema.TypeBool,
-				Description: resourceYandexYDBDatabaseDedicated().Schema["tls_enabled"].Description,
-				Computed:    true,
-			},
-
-			"created_at": {
-				Type:        schema.TypeString,
-				Description: common.ResourceDescriptions["created_at"],
-				Computed:    true,
-			},
-
-			"status": {
-				Type:        schema.TypeString,
-				Description: resourceYandexYDBDatabaseDedicated().Schema["status"].Description,
-				Computed:    true,
-			},
-
-			"deletion_protection": {
-				Type:        schema.TypeBool,
-				Description: common.ResourceDescriptions["deletion_protection"],
-				Optional:    true,
-				Computed:    true,
-			},
-		},
+	dataSource.Schema["database_id"] = &schema.Schema{
+		Type:        schema.TypeString,
+		Description: "ID of the Yandex Database cluster.",
+		Optional:    true,
 	}
+
+	delete(dataSource.Schema, "sleep_after")
+
+	return dataSource
 }
 
 func dataSourceYandexYDBDatabaseDedicatedRead(d *schema.ResourceData, meta interface{}) error {
