8c6db9ccda30b2b305448891e647ea09654e558c
shapiaev <shapiaev@yandex-team.com>
2026-05-20 12:55:37 +0300

    ClickHouse: Terraform Provider: fix connection manager known after apply commit_hash:92ffc57d77fece5f8ca7951ae2056af6ba29424d

--- yandex-framework/services/mdb_clickhouse_user/resource_schema.go
+++ yandex-framework/services/mdb_clickhouse_user/resource_schema.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework-validators/setvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/booldefault"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/objectplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
@@ -799,5 +800,8 @@ func ConnectionManagerSchema() schema.SingleNestedAttribute {
 			},
 		},
 		Computed: true,
+		PlanModifiers: []planmodifier.Object{
+			objectplanmodifier.UseStateForUnknown(),
+		},
 	}
 }
